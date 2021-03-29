cc396683b9bb8c75abdc7ba225f954bf59462192
Cactiw <Knyazev1514@gmail.com>
2021-03-29 15:49:26 +0300

    Some interface fixes

--- Bank/MainWindow.xaml.cs
+++ Bank/MainWindow.xaml.cs
@@ -132,7 +132,7 @@ namespace Bank
                 text += "\nКлиентов ушло: " + Experiment.Department.ClientsLeft;
                 text += "\nПолучено денег: " + Experiment.Department.EarnedMoney;
                 text += "\nВыплачено зарплат: " + (int)Experiment.Department.SalaryPaid;
-                text += "\nИтого прибыль:" + (int)(Experiment.Department.EarnedMoney - Experiment.Department.SalaryPaid);
+                text += "\nИтого прибыль: " + (int)(Experiment.Department.EarnedMoney - Experiment.Department.SalaryPaid);
                 text += "\nКоэффициент занятости: " + Experiment.Department.GetWorkersBusyStatistics().ToString("0.00");
             }
             StatisticsBlock.Text = text;
